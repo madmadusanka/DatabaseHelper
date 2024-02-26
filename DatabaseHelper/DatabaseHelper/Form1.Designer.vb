1ddb2d91399aa0c50a94b8c51f09baa9e0378652
PasinduGayashan <pasindugayashan63@gmail.com>
2024-02-26 18:03:46 +0530

    Rename to My project name to DatabaseHelper

--- DatabaseHelper/DatabaseHelper/Form1.Designer.vb
+++ DatabaseHelper/DatabaseHelper/Form1.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
-Partial Class Form1
+Partial Class frmMain
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
@@ -22,10 +22,106 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        Panel1 = New Panel()
+        lblDatabaseCount = New Label()
+        btnConnectServer = New Button()
+        lblServerName = New Label()
+        txtServerName = New TextBox()
+        Panel2 = New Panel()
+        lblsqlserver = New Label()
+        Panel1.SuspendLayout()
+        Panel2.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' Panel1
+        ' 
+        Panel1.BackColor = SystemColors.ActiveBorder
+        Panel1.Controls.Add(lblDatabaseCount)
+        Panel1.Controls.Add(btnConnectServer)
+        Panel1.Controls.Add(lblServerName)
+        Panel1.Controls.Add(txtServerName)
+        Panel1.Controls.Add(Panel2)
+        Panel1.Dock = DockStyle.Left
+        Panel1.Location = New Point(0, 0)
+        Panel1.Name = "Panel1"
+        Panel1.Size = New Size(311, 450)
+        Panel1.TabIndex = 0
+        ' 
+        ' lblDatabaseCount
+        ' 
+        lblDatabaseCount.AutoSize = True
+        lblDatabaseCount.Location = New Point(12, 146)
+        lblDatabaseCount.Name = "lblDatabaseCount"
+        lblDatabaseCount.Size = New Size(104, 15)
+        lblDatabaseCount.TabIndex = 4
+        lblDatabaseCount.Text = "SQL Server Name :"
+        ' 
+        ' btnConnectServer
+        ' 
+        btnConnectServer.Location = New Point(12, 109)
+        btnConnectServer.Name = "btnConnectServer"
+        btnConnectServer.Size = New Size(287, 23)
+        btnConnectServer.TabIndex = 3
+        btnConnectServer.Text = "Connect"
+        btnConnectServer.UseVisualStyleBackColor = True
+        ' 
+        ' lblServerName
+        ' 
+        lblServerName.AutoSize = True
+        lblServerName.Location = New Point(12, 62)
+        lblServerName.Name = "lblServerName"
+        lblServerName.Size = New Size(104, 15)
+        lblServerName.TabIndex = 2
+        lblServerName.Text = "SQL Server Name :"
+        ' 
+        ' txtServerName
+        ' 
+        txtServerName.Location = New Point(12, 81)
+        txtServerName.Name = "txtServerName"
+        txtServerName.Size = New Size(287, 23)
+        txtServerName.TabIndex = 1
+        ' 
+        ' Panel2
+        ' 
+        Panel2.BackColor = SystemColors.ControlDarkDark
+        Panel2.Controls.Add(lblsqlserver)
+        Panel2.Location = New Point(0, 0)
+        Panel2.Name = "Panel2"
+        Panel2.Size = New Size(311, 48)
+        Panel2.TabIndex = 0
+        ' 
+        ' lblsqlserver
+        ' 
+        lblsqlserver.AutoSize = True
+        lblsqlserver.Font = New Font("Segoe UI Semibold", 12.0F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        lblsqlserver.ForeColor = SystemColors.ButtonFace
+        lblsqlserver.Location = New Point(112, 12)
+        lblsqlserver.Name = "lblsqlserver"
+        lblsqlserver.Size = New Size(91, 21)
+        lblsqlserver.TabIndex = 0
+        lblsqlserver.Text = "SQL Server"
+        ' 
+        ' frmMain
+        ' 
+        AutoScaleDimensions = New SizeF(7.0F, 15.0F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(851, 450)
+        Controls.Add(Panel1)
+        Name = "frmMain"
+        Text = "Main "
+        Panel1.ResumeLayout(False)
+        Panel1.PerformLayout()
+        Panel2.ResumeLayout(False)
+        Panel2.PerformLayout()
+        ResumeLayout(False)
     End Sub
+
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents lblServerName As Label
+    Friend WithEvents txtServerName As TextBox
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents lblsqlserver As Label
+    Friend WithEvents btnConnectServer As Button
+    Friend WithEvents lblDatabaseCount As Label
 
 End Class
--- DatabaseHelper/DatabaseHelper/Form1.vb
+++ DatabaseHelper/DatabaseHelper/Form1.vb
@@ -1,3 +1,6 @@
-﻿Public Class Form1
-
+﻿Public Class frmMain
+    Private Sub btnConnectServer_Click(sender As Object, e As EventArgs) Handles btnConnectServer.Click
+        Dim connector As New Connection()
+        connector.ConnectServer(txtServerName.Text, lblDatabaseCount)
+    End Sub
 End Class
