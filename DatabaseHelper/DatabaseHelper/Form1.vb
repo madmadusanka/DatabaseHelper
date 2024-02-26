327f5431445584f2517959f14702ea7a7d3388e4
PasinduGayashan <pasindugayashan63@gmail.com>
2024-02-26 19:36:56 +0530

    Revert "Rename to My project name to DatabaseHelper"
    This reverts commit 1ddb2d91399aa0c50a94b8c51f09baa9e0378652.

--- DatabaseHelper/DatabaseHelper/Form1.vb
+++ DatabaseHelper/DatabaseHelper/Form1.vb
@@ -1,6 +1,3 @@
-﻿Public Class frmMain
-    Private Sub btnConnectServer_Click(sender As Object, e As EventArgs) Handles btnConnectServer.Click
-        Dim connector As New Connection()
-        connector.ConnectServer(txtServerName.Text, lblDatabaseCount)
-    End Sub
+﻿Public Class Form1
+
 End Class
