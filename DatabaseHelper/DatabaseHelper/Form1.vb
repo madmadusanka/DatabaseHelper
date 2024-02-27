6cba8c7fd1c403859d79eb5ef9661717c93894b3
PasinduGayashan <pasindugayashan63@gmail.com>
2024-02-27 12:00:50 +0530

    Setup Landing Page
    Create landing page and SQL Server connection functionality.

--- DatabaseHelper/DatabaseHelper/Form1.vb
+++ DatabaseHelper/DatabaseHelper/Form1.vb
@@ -1,3 +1,26 @@
-﻿Public Class Form1
+﻿Public Class frmMain
+    Private isConnected As Boolean = False
+    Private connector As New Connection()
 
+    Private Async Sub btnToggleConnection_Click(sender As Object, e As EventArgs) Handles btnToggleConnection.Click
+        If isConnected Then
+            ' Disconnect from the server
+            DisconnectFromServer()
+        Else
+            ' Connect to the server
+            Await ConnectToServer()
+        End If
+    End Sub
+
+    Private Async Function ConnectToServer() As Task
+        Await connector.ConnectServer(txtServerName.Text, lblDatabaseCount, btnToggleConnection)
+        isConnected = True
+    End Function
+
+    Private Sub DisconnectFromServer()
+        ' Perform disconnect logic here if necessary
+        lblDatabaseCount.Text = ""
+        isConnected = False
+        btnToggleConnection.Text = "Connect"
+    End Sub
 End Class
